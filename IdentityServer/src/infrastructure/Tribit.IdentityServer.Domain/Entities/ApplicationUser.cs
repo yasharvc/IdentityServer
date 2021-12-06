2f35f803cb1b872612e6f8ad3bda0dfda0584aee
yasharvc <yasharvc@yahoo.com>
2021-12-06 21:20:19 +0300

    Replaced project inside correct folder and made context of ApplicationUser not default Identityuser

---
+++ IdentityServer/src/infrastructure/Tribit.IdentityServer.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Tribit.IdentityServer.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
